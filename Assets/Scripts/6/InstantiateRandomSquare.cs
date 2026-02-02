b400ab1ba07fe7c065a666805da62e456dc67093
AriiqAlhafizh <ariiqalhafizh69@gmail.com>
2026-02-03 05:54:13 +0700

    Scene 6

---
+++ Assets/Scripts/6/InstantiateRandomSquare.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class InstantiateRandomSquare : MonoBehaviour
+{
+    [SerializeField] private GameObject environment;
+    [SerializeField] private GameObject squarePrefab;
+
+    private float minX, maxX, minY, maxY;
+
+    private void Awake()
+    {
+        setBounds();
+    }
+
+    void Start()
+    {
+        int randomValue = Random.RandomRange(1, 10);
+        for (int i = 0; i < randomValue; i++)
+        {
+            InstantiateSquareAtRandomPosition();
+        }
+    }
+
+    void setBounds()
+    {
+        Collider2D[] colliders = environment.GetComponentsInChildren<Collider2D>();
+
+        Bounds totalBounds = colliders[0].bounds;
+
+        for (int i = 1; i < colliders.Length; i++)
+        {
+            totalBounds.Encapsulate(colliders[i].bounds);
+        }
+
+        minX = totalBounds.min.x;
+        maxX = totalBounds.max.x;
+        minY = totalBounds.min.y;
+        maxY = totalBounds.max.y;
+    }
+
+    private void InstantiateSquareAtRandomPosition()
+    {
+        Vector2 randomPoint = new Vector2(
+            Random.Range(minX, maxX),
+            Random.Range(minY, maxY)
+        );
+        Instantiate(squarePrefab, randomPoint, Quaternion.identity, this.transform);
+    }
+}
